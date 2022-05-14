4991cad9bed3cedd8371f1d4203dda7567f1367e
Facundo Tarelli <60760247+Facundotarelli@users.noreply.github.com>
2022-05-14 18:08:47 -0300

    Entrega de TP_2
    TP_2

---
+++ TP_2/src/herramientas.h
@@ -0,0 +1,17 @@
+/*
+ * herramientas.h
+ *
+ *  Created on: 13 may. 2022
+ *      Author: Facundo Tarelli
+ */
+
+#ifndef HERRAMIENTAS_H_
+#define HERRAMIENTAS_H_
+
+float totalDelArray(Passenger* valores, int longitud);
+
+float promedioDeArray(Passenger* valores, int longitud, float total);
+
+int contadorDelMayorAlPromedio(Passenger* valores, int longitud, float promedio);
+
+#endif /* HERRAMIENTAS_H_ */
